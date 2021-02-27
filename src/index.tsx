61ad2b905fd3d2d4913e888e8dd0592fc07a74a2
Salas <gae.m.project@gmail.com>
2021-02-27 17:32:42 +0800

    update research and bio sections

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactGA.initialize("UA-82808777-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
